ae2bcccb91921048f875ec1767be423a619a90eb
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-02-24 09:28:53 +0100

    Added Screen

---
+++ include/screen.h
@@ -0,0 +1,27 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include "display.h"
+
+typedef enum {
+    CPG_LOOP,
+    CPG_MAIN_MENU,
+    CPG_OPTIONS,
+    CPG_GAME,
+    CPG_EXIT
+} ExitCode;
+
+typedef struct CPG_Screen CPG_Screen;
+
+struct CPG_Screen {
+    CPG_Display* display;
+    void (*loop)(CPG_Screen*);
+    ExitCode code;
+};
+
+CPG_Screen* CPG_Screen_CreateNew(
+        CPG_Display* display, 
+        void (*loop)(CPG_Screen*));
+void CPG_Screen_RunLoop(CPG_Screen* self);
+
+#endif // SCREEN_H
---
+++ src/screen.c
@@ -0,0 +1,32 @@
+#include "../include/screen.h"
+
+CPG_Screen* CPG_Screen_CreateNew(
+        CPG_Display* display, 
+        void (*loop)(CPG_Screen*)) {
+
+   CPG_Screen* screen = (CPG_Screen*) malloc(sizeof(CPG_Screen)); 
+   screen->display = display;
+   screen->loop = loop;
+   screen->code = CPG_LOOP;
+   
+   return screen;
+}
+
+void CPG_Screen_Update(CPG_Screen* self) {
+    while (self->code == CPG_LOOP) {
+        self->loop(self);
+    }
+}
+
+void CPG_Screen_HandleExitCode(CPG_Screen* self) {
+    switch (self->code) {
+        // TODO Exit code handling
+        default:
+            break;
+    }
+}
+
+void CPG_Screen_RunLoop(CPG_Screen* self) {
+    CPG_Screen_Update(self);
+    CPG_Screen_HandleExitCode(self);
+}
